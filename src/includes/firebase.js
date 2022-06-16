b585968c4dd0eba7d31f41f01b18fcf0bfa58152
dulshaniko7 <dulshaniko7@gmail.com>
2022-06-16 22:24:23 +0530

    remove firebase

--- src/includes/firebase.js
+++ src/includes/firebase.js
@@ -1,12 +1,11 @@
 /* eslint-disable */
-import firebase from 'firebase/app'
+import firebase from 'firebase'
 
 const firebaseConfig = {
   apiKey: "AIzaSyAkDaKn384JligfHl004evSYq3a-HacQBU",
   authDomain: "my-music-d0e43.firebaseapp.com",
   projectId: "my-music-d0e43",
   storageBucket: "my-music-d0e43.appspot.com",
-  messagingSenderId: "764354504257",
   appId: "1:764354504257:web:f30158ac601447a013c8c0",
   measurementId: "G-BZQBFT5SNP"
 };
